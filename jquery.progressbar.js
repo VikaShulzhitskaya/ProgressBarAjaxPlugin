f640b9f94f01d0a56d7d9635e6b0ceb8bd0eecd8
VikaShulzhitskaya <vikaprevet@gmail.com>
2016-07-28 11:39:47 +0300

    Added upload listener

--- jquery.progressbar.js
+++ jquery.progressbar.js
@@ -4,25 +4,16 @@
 
     options = $.extend({
       buttonId: '#button',
-      url: 'http://download.thinkbroadband.com/20MB.zip',
-      type: 'GET',
-      contentType: false,
-      processData: false,
-      successFunction: function () {
-        alert('Done!');
-      }
+      ajaxSet: {}
     }, options);
 
     function make() {
       var progressbar = (this);
       var $button = $(options.buttonId);
+      var ajaxOptions = options.ajaxSet;
       $button.on('click', function (event) {
         event.preventDefault();
-        $.ajax({
-          url: options.url,
-          type: options.type,
-          contentType: options.contentType,
-          processData: options.processData,
+        ajaxOptions = $.extend({
           xhr: function () {
             var xhr = $.ajaxSettings.xhr();
             xhr.addEventListener('progress', function (evt) {
@@ -31,16 +22,20 @@
                 $(progressbar).val(percentComplete);
               }
             }, false);
+            xhr.upload.addEventListener('progress', function (evt) {
+              if (evt.lengthComputable) {
+                var percentComplete = Math.ceil(evt.loaded / evt.total * 100);
+                $(progressbar).val(percentComplete);
+              }
+            }, false);
             return xhr;
-          },
-          success: options.successFunction
+          }
+        }, options.ajaxSet);
 
-        });
+        $.ajax(ajaxOptions);
       });
 
     }
-
-
 
     return this.each(make);
 
--- testingScript.js
+++ testingScript.js
@@ -1,18 +1,14 @@
-$(function(){
-	var progressBar = $('#progressbar1').progressbarPlugin({
-		buttonId: '#get',
-		successFunction: success
-
-
-	});
-/* A function to be called if the request succeeds. 
-The function gets passed three arguments: 
- - The data returned from the server, formatted 
-according to the dataType parameter 
-or the dataFilter callback function, if specified; 
- - a string describing the status; 
- - and the jqXHR */
-	function success(data, textStatus, jqXHR){
-		alert('hey');
-	}
+$(function () {
+  var progressBar = $('#progressbar1').progressbarPlugin({
+    buttonId: '#get',
+    ajaxSet: {
+      url: 'http://download.thinkbroadband.com/20MB.zip',
+      type: 'GET',
+      contentType: false,
+      processData: false,
+      success: function () {
+        alert('Done!');
+      }
+    }
+  });
 });
